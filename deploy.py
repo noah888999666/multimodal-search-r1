020e74999d74df1cfdaceccfb11f8ac96f6e6280
noah888999666 <noah888999666@gmail.com>
2025-07-22 18:31:46 +0800

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -70,7 +70,7 @@ wandb login $WANDB_API_KEY
 
 pip install huggingface_hub
 #huggingface-cli logout
-export HF_TOKEN=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
+export HF_TOKEN=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
 huggingface-cli login --token $HF_TOKEN
 # pip install hydra-core
 # pip install ray
